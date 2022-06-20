3cc0c4df72916649df57a0a365b5a5da1b8fdb9a
Eilis Jung <eilis-jung@protonmail.com>
2022-06-20 14:13:11 +0200

    temp

---
+++ VulkanPractice/VulkanPractice.cpp
@@ -0,0 +1,12 @@
+﻿// VulkanPractice.cpp : Defines the entry point for the application.
+//
+
+#include "VulkanPractice.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
